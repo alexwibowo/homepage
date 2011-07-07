92973a351540abc5fa1610916bacc98c0db9325c
Alex Wibowo <alexwibowo at gee-mail dot com>
2011-07-07 22:40:45 +1000

    Initial commit of homepage

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homepage::Application.config.secret_token = 'c2f23bd0205b7ff40e6b0cad5430b2824f3a6a44b5fdeeba8b7c2f5037acd1a9bc74b41d851a8337247786b38c28dc2992217e87d292bb8f1c70d23b3a369c17'
